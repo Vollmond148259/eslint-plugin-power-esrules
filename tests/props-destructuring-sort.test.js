7a3e24c85815dcd0fb4d58120e9e0616b0458512
Vollmond148259 <Vollmond148259@gmail.com>
2026-06-04 18:07:56 +0300

    fix: фикс data-testid и тест

--- tests/props-destructuring-sort.test.js
+++ tests/props-destructuring-sort.test.js
@@ -11,42 +11,42 @@ const ruleTester = new RuleTester({
 
 ruleTester.run("props-destructuring-sort", rule, {
   valid: [
-  {
-    name: "sorted destructuring from props in functional component",
-    code: `
+    {
+      name: "sorted destructuring from props in functional component",
+      code: `
       function MyComponent(props) {
         const { alpha, beta } = props;
         return <div>{alpha}{beta}</div>;
       }
     `,
-  },
-  {
-    name: "single property from props",
-    code: `
+    },
+    {
+      name: "single property from props",
+      code: `
       function MyComponent(props) {
         const { only } = props;
         return <div>{only}</div>;
       }
     `,
-  },
-  {
-    name: "unsorted props outside React component",
-    code: `
+    },
+    {
+      name: "unsorted props outside React component",
+      code: `
       function util(props) {
         const { z, a } = props;
         return z + a;
       }
     `,
-  },
-  {
-    name: "sorted params in arrow component",
-    code: `
+    },
+    {
+      name: "sorted params in arrow component",
+      code: `
       const MyComponent = ({ alpha, beta }) => <div>{alpha}{beta}</div>;
     `,
-  },
-  {
-    name: "sorted this.props in class component",
-    code: `
+    },
+    {
+      name: "sorted this.props in class component",
+      code: `
       class MyComponent extends React.Component {
         render() {
           const { alpha, beta } = this.props;
@@ -54,62 +54,62 @@ ruleTester.run("props-destructuring-sort", rule, {
         }
       }
     `,
-  },
-  {
-    name: "rest element last when already sorted",
-    code: `
+    },
+    {
+      name: "rest element last when already sorted",
+      code: `
       function MyComponent(props) {
         const { alpha, beta, ...rest } = props;
         return <div>{alpha}</div>;
       }
     `,
-  },
+    },
   ],
 
   invalid: [
-  {
-    name: "const from props in functional component",
-    code: `
+    {
+      name: "const from props in functional component",
+      code: `
       function MyComponent(props) {
         const { zebra, alpha } = props;
         return <div>{zebra}{alpha}</div>;
       }
     `,
-    output: `
+      output: `
       function MyComponent(props) {
         const { alpha, zebra } = props;
         return <div>{zebra}{alpha}</div>;
       }
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
-  {
-    name: "destructured params in function declaration",
-    code: `
+      errors: [{ messageId: "incorrectOrder" }],
+    },
+    {
+      name: "destructured params in function declaration",
+      code: `
       function MyComponent({ zebra, alpha }) {
         return <div>{zebra}{alpha}</div>;
       }
     `,
-    output: `
+      output: `
       function MyComponent({ alpha, zebra }) {
         return <div>{zebra}{alpha}</div>;
       }
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
-  {
-    name: "destructured params in arrow component with JSX",
-    code: `
+      errors: [{ messageId: "incorrectOrder" }],
+    },
+    {
+      name: "destructured params in arrow component with JSX",
+      code: `
       const MyComponent = ({ zebra, alpha }) => <div>{zebra}{alpha}</div>;
     `,
-    output: `
+      output: `
       const MyComponent = ({ alpha, zebra }) => <div>{zebra}{alpha}</div>;
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
-  {
-    name: "this.props in class component",
-    code: `
+      errors: [{ messageId: "incorrectOrder" }],
+    },
+    {
+      name: "this.props in class component",
+      code: `
       class MyComponent extends Component {
         render() {
           const { zebra, alpha } = this.props;
@@ -117,7 +117,7 @@ ruleTester.run("props-destructuring-sort", rule, {
         }
       }
     `,
-    output: `
+      output: `
       class MyComponent extends Component {
         render() {
           const { alpha, zebra } = this.props;
@@ -125,33 +125,35 @@ ruleTester.run("props-destructuring-sort", rule, {
         }
       }
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
-  {
-    name: "rest stays last after sort",
-    code: `
+      errors: [{ messageId: "incorrectOrder" }],
+    },
+    {
+      name: "rest stays last after sort",
+      code: `
       function MyComponent(props) {
         const { zebra, alpha, ...rest } = props;
         return <div>{zebra}</div>;
       }
     `,
-    output: `
+      output: `
       function MyComponent(props) {
         const { alpha, zebra, ...rest } = props;
         return <div>{zebra}</div>;
       }
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
-  {
-    name: "memo-wrapped component params",
-    code: `
+      errors: [{ messageId: "incorrectOrder" }],
+    },
+    {
+      name: "memo-wrapped component params",
+      code: `
       const MyComponent = memo(({ zebra, alpha }) => <div>{zebra}</div>);
     `,
-    output: `
+      output: `
       const MyComponent = memo(({ alpha, zebra }) => <div>{zebra}</div>);
     `,
-    errors: [{ messageId: "incorrectOrder" }],
-  },
+      errors: [{ messageId: "incorrectOrder" }],
+    },
   ],
 });
+
+console.log("OK");
---
+++ tests/require-data-testid.test.js
@@ -0,0 +1,112 @@
+const { RuleTester } = require("eslint");
+const rule = require("../lib/rules/require-data-testid");
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+});
+
+ruleTester.run("require-data-testid", rule, {
+  valid: [
+    {
+      name: "function declaration with data-testid",
+      code: `
+        function MyComponent() {
+          return <div data-testid="my-component">Hello</div>;
+        }
+      `,
+    },
+    {
+      name: "arrow component with dataTestID",
+      code: `
+        const MyComponent = () => <section dataTestID="section">Hi</section>;
+      `,
+    },
+    {
+      name: "memo-wrapped component with data-testid",
+      code: `
+        const MyComponent = memo(() => <div data-testid="memo">Memo</div>);
+      `,
+    },
+    {
+      name: "forwardRef component with data-testid",
+      code: `
+        const MyComponent = forwardRef((props, ref) => <div ref={ref} data-testid="ref">Ref</div>);
+      `,
+    },
+    {
+      name: "nested JSX inside Fragment",
+      code: `
+        const MyComponent = () => (
+          <>
+            <div data-testid="inside">Hello</div>
+          </>
+        );
+      `,
+    },
+    {
+      name: "non-PascalCase function is ignored",
+      code: `
+        function utilFunction() {
+          return <div>Hello</div>;
+        }
+      `,
+    },
+  ],
+
+  invalid: [
+    {
+      name: "function declaration without data-testid",
+      code: `
+        function MyComponent() {
+          return <div>Hello</div>;
+        }
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+    {
+      name: "arrow component missing data-testid",
+      code: `
+        const MyComponent = () => <section>Hi</section>;
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+    {
+      name: "memo-wrapped component missing data-testid",
+      code: `
+        const MyComponent = memo(() => <div>Memo</div>);
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+    {
+      name: "forwardRef component missing data-testid",
+      code: `
+        const MyComponent = forwardRef((props, ref) => <div ref={ref}>Ref</div>);
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+    {
+      name: "nested JSX in Fragment missing data-testid",
+      code: `
+        const MyComponent = () => (
+          <>
+            <div>Hello</div>
+          </>
+        );
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+    {
+      name: "HOC wrapped component without data-testid",
+      code: `
+        const MyComponent = observer(() => <div>Hello</div>);
+      `,
+      errors: [{ messageId: "missingDataTestId" }],
+    },
+  ],
+});
+
+console.log("OK");
